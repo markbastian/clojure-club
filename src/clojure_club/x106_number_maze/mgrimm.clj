12dd84d7050f813fc59098fa15e139c9b7ce6aa7
Matt Grimm <matt.t.grimm@gmail.com>
2017-03-14 13:37:16 -0600

    Show operations in result

--- src/clojure_club/x106_number_maze/mgrimm.clj
+++ src/clojure_club/x106_number_maze/mgrimm.clj
@@ -5,10 +5,10 @@
   (loop [seen #{} q (conj PersistentQueue/EMPTY [start 1 [start]])]
     (let [[x depth path] (peek q)]
       (if (= x end)
-        [depth (->> q seq (filter (fn [[y d p]] (and (= y end) (= d depth)))) (map last))]
+        [depth (->> q seq (filter (fn [[y d p]] (and (= y end) (= d depth)))) (map #(nth % 2)))]
         (recur (conj seen x)
                (let [a (+ x 2) b (* x 2) c (/ x 2)]
                  (cond-> (pop q)
-                         (not (seen a)) (conj [a (inc depth) (conj path a)])
-                         (not (seen b)) (conj [b (inc depth) (conj path b)])
-                         (not (or (seen c) (odd? x))) (conj [c (inc depth) (conj path c)]))))))))
+                   (not (seen a)) (conj [a (inc depth) (conj path :add2 a)])
+                   (not (seen b)) (conj [b (inc depth) (conj path :doub b)])
+                   (not (or (seen c) (odd? x))) (conj [c (inc depth) (conj path :halv c)]))))))))
