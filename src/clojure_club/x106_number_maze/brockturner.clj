090cd2add38fd39080eefac47f11e4b9beb060e0
VoltaicAge8838 <brocam@gmail.com>
2017-03-14 19:38:49 -0600

    Initial function

---
+++ src/clojure_club/x106_number_maze/brockturner.clj
@@ -0,0 +1,19 @@
+(ns clojure-club.x106-number-maze.brockturner)
+
+(defn maze-runner [start end]
+  (loop [current #{start}
+         past #{}
+         dist 1]
+    (if (current end)
+      dist
+      (recur (->> (reduce
+                   (fn [nset n]
+                     (let [tmp (conj nset (+ 2 n) (* 2 n))]
+                       (if (= 0 (mod n 2))
+                         (conj tmp (/ n 2))
+                         tmp)))
+                   #{} current)
+                  (remove (into past current))
+                  (apply hash-set))
+             (into past current)
+             (inc dist)))))
