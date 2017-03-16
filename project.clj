8355a71f55989c328af2bfb26b45108da427c5c7
Mark Bastian <markbastian@gmail.com>
2017-03-15 22:00:42 -0600

    Added cljs plugin.

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+                 [org.clojure/clojurescript "1.9.494"]]
+
+  :plugins [[lein-cljsbuild "1.1.5"]]
 
   :source-paths ["src/main/clj" "src/main/cljc"]
   :test-paths ["src/test/clj" "src/test/cljc"]
