734b3486cbdc93a8b7f1a230470a36bee72cbc61
Mark Bastian <markbastian@gmail.com>
2017-03-16 23:29:29 -0600

    Added general solver.

--- src/main/clj/clojure_club/x106_number_maze/NOTES.md
+++ src/main/clj/clojure_club/x106_number_maze/NOTES.md
@@ -1,14 +1,45 @@
 
 # Observations
-* Recursion
+
+---
+
+#Maybe New?
+* PersistentQueue/EMPTY
+  * peek & pop like [], but FIFO vs. LIFO
+  * No literal syntax
+* Multiple Arities
+* Rolling up a map
+  * ```(take-while identity (iterate {1 2 2 3 3 4 4 5} 1))```
+* Strings, keywords, & symbols to record ops
+* juxt, cond->, even?, letfn
+
+---
+
+#Data Representation
+* ```[[1 2] [1 3]] ;Vector of vectors```
+  * Works well for generational BFS
+  * Implicit tree, but no lookup
+* ```{:path [[s]] :visited #{s} :count 0 :path-count}```
+  * Easy to communicate intent
+  * Trivial to assoc in new data
 
 ----
 
-#Recursion
+#Search Space Representation
+* PersistentQueue/EMPTY
+* [[4 8] [4 6] [4 2]]
+* seq, {}, vs. #{} for visited
 
-```
-(defn r[x] (if (pos? x) x (r (inc x))))
-```
+----
+
+#Iterating
+* recursion
+* loop-recur
+* iterate
+
+---
+
+#Recursion
 
 * Can be easy to reason about
 * The stack will grow
@@ -17,14 +48,6 @@
 ---
 
 #loop-recur
-
-```
-(defn lr [x]
-  (loop [r x]
-    (if (pos? r)
-      r
-      (recur (inc r)))))
-```
 
 * Tail-call recursion
 * Straightforward to set up complicated values
@@ -40,15 +63,6 @@
     * take, take-while, drop, drop-while, split-with, nth
 
 ---
-
-#Clojure 
-
-<pre class="clojure" style="font-size:16px;width: 100%;left: 0%;;margin:auto">
-<code>(defn number-maze[s g]
-        (letfn[(nbrs[x] (cond-> [(* 2 x) (+ x 2)] (even? x) (conj (/ x 2))))]
-          (inc (count (take-while #(not (% g)) (iterate #(reduce into #{} (map nbrs %)) #{s}))))))</code></pre>
-
-----
 
 #Comparison
 
--- src/main/clj/clojure_club/x106_number_maze/mbastian.clj
+++ src/main/clj/clojure_club/x106_number_maze/mbastian.clj
@@ -1,4 +1,5 @@
 (ns clojure-club.x106-number-maze.mbastian
+  (:require [clojure.string :as cs])
   (:import (clojure.lang PersistentQueue)))
 
 ;Produce only the number of steps.
@@ -6,12 +7,12 @@
   (letfn[(nbrs[x] (cond-> [(* 2 x) (+ x 2)] (even? x) (conj (/ x 2))))]
     (inc (count (take-while #(not (% g)) (iterate #(reduce into #{} (map nbrs %)) #{s}))))))
 
-(time (number-maze 1 4137))
-(time (number-maze 7 43))
-(time (number-maze 9 2))
-(time (number-maze 2 9))
-(time (number-maze 2 4))
-(time (number-maze 2 2))
+;(time (number-maze 1 4137))
+;(time (number-maze 7 43))
+;(time (number-maze 9 2))
+;(time (number-maze 2 9))
+;(time (number-maze 2 4))
+;(time (number-maze 2 2))
 
 ;Return a single path
 (defn number-path [s g]
@@ -22,12 +23,12 @@
         (v n) (recur v (pop q))
         :default (recur (conj v edge) (into (pop q) (map (fn [x] [x n]) (nbrs n))))))))
 
-(time (number-path 1 4137))
-(time (number-path 7 43))
-(time (number-path 9 2))
-(time (number-path 2 9))
-(time (number-path 2 4))
-(time (number-path 2 2))
+;(time (number-path 1 4137))
+;(time (number-path 7 43))
+;(time (number-path 9 2))
+;(time (number-path 2 9))
+;(time (number-path 2 4))
+;(time (number-path 2 2))
 
 ;Return them all - very slow
 (defn number-paths[s g]
@@ -39,11 +40,11 @@
          (filter (comp #{g} peek)))))
 
 ;(time (number-paths 1 4137))
-(time (number-paths 7 43))
-(time (number-paths 9 2))
-(time (number-paths 2 9))
-(time (number-paths 2 4))
-(time (number-paths 2 2))
+;(time (number-paths 7 43))
+;(time (number-paths 9 2))
+;(time (number-paths 2 9))
+;(time (number-paths 2 4))
+;(time (number-paths 2 2))
 
 ;Return them all
 (defn fast-number-paths[s g]
@@ -56,12 +57,12 @@
            (some (fn [{:keys [paths]}] (when (some pred paths) paths)))
            (filter pred)))))
 
-(time (fast-number-paths 1 4137))
-(time (fast-number-paths 7 43))
-(time (fast-number-paths 9 2))
-(time (fast-number-paths 2 9))
-(time (fast-number-paths 2 4))
-(time (fast-number-paths 2 2))
+;(time (fast-number-paths 1 4137))
+;(time (fast-number-paths 7 43))
+;(time (fast-number-paths 9 2))
+;(time (fast-number-paths 2 9))
+;(time (fast-number-paths 2 4))
+;(time (fast-number-paths 2 2))
 
 ;The below code deconstructs the above function to demonstrate how easy it is to
 ; assemble/dissassemble and debug Clojure forms.
@@ -89,9 +90,77 @@
          (some (partial stop-cond pred))
          (filter pred))))
 
-(time (fast-number-paths-deconstructed 1 4137))
-(time (fast-number-paths-deconstructed 7 43))
-(time (fast-number-paths-deconstructed 9 2))
-(time (fast-number-paths-deconstructed 2 9))
-(time (fast-number-paths-deconstructed 2 4))
-(time (fast-number-paths-deconstructed 2 2))
+;(time (fast-number-paths-deconstructed 1 4137))
+;(time (fast-number-paths-deconstructed 7 43))
+;(time (fast-number-paths-deconstructed 9 2))
+;(time (fast-number-paths-deconstructed 2 9))
+;(time (fast-number-paths-deconstructed 2 4))
+;(time (fast-number-paths-deconstructed 2 2))
+
+;;;;;;;;;;;;;;;;;;Generalized Solver;;;;;;;;;;;;;;;;;;;;;;;
+(defn expand [{v :visited [[n :as edge]] :queue nbrs :neighbors :as m}]
+  (cond-> (update m :queue pop) (not (v n))
+          (->
+            (update :visited conj edge)
+            (update :queue into (map (fn [x] [x n]) (nbrs n))))))
+
+(defn bfs-expander [{:keys[start neighbors]}]
+  (iterate expand {:visited {}
+                   :queue (conj PersistentQueue/EMPTY [start :done])
+                   :neighbors neighbors}))
+
+(defn rsolve-path [goal {:keys [visited]}]
+  (when (visited goal) (reverse (take-while (complement #{:done}) (iterate visited goal)))))
+
+(defn bfs-solve [& {:keys [goal] :as ic}]
+  (some (fn [{:keys [queue] :as m}]
+          (or (rsolve-path goal m) (when (empty? queue) :no-solution)))
+        (bfs-expander ic)))
+
+;Solve today's problem
+(bfs-solve
+  :start 7
+  :goal 43
+  :neighbors (fn nbrs [x] (cond-> [(+ 2 x) (* 2 x)] (even? x) (conj (/ x 2)))))
+
+;Use another function to generate neighbors
+(bfs-solve
+  :start 1
+  :goal 7
+  :neighbors {1 [2] 2 [3] 3 [4]})
+
+;Get creative....
+(def dungeon
+  ["XXXXXXXX XXXXXXXX"
+   "X     XX XX    TX"
+   "X XXX       XXXXX"
+   "X XXXX XX XXXXX X"
+   "X  XXX XXXX     X"
+   "XX  XXXX    XXX X"
+   "XXX      XXXXXX X"
+   "XXS   XXXXXXXXX X"])
+
+(defn find-cell [d sym]
+  (first (for [i (range (count d)) j (range (count (d i)))
+               :when (= sym (get-in d [i j]))] [i j])))
+
+(defn square-neighbors [[i j]]
+  (map vector
+       ((juxt inc identity dec identity) i)
+       ((juxt identity inc identity dec) j)))
+
+(defn dungeon-neigbors [d c]
+  (filter #(#{\space \S \T} (get-in d %)) (square-neighbors c)))
+
+(def dungeon-path
+  (bfs-solve
+    :start (find-cell dungeon \S)
+    :goal (find-cell dungeon \T)
+    :neighbors (partial dungeon-neigbors dungeon)))
+
+(if (not= :no-solution dungeon-path)
+  (map cs/join
+       (reduce #(assoc-in %1 %2 \.)
+               (mapv vec dungeon)
+               (rest (butlast dungeon-path))))
+  "No solution!")
