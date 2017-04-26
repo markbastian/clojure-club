07fb0680be3906917a761c869b5803ee18c48346
Mark Bastian <markbastian@gmail.com>
2017-04-25 23:00:05 -0600

    tweaks

--- src/main/clj/clojure_club/gurps/mbastian.clj
+++ src/main/clj/clojure_club/gurps/mbastian.clj
@@ -7,14 +7,6 @@
 
 (def x (->> "clojure_club/gurps/basic_set.adq" io/resource slurp xml/parse-str))
 
-#_(clojure.walk/prewalk
-  (fn [{:keys [tag attrs content] :as m}]
-    (cond
-      (string? content) {tag content}
-      (seq content) {tag m}
-      :else m))
-  x)
-
 (defn numeric-attr [{:keys [tag content]}]
   {tag (some-> content first)})
 
@@ -35,7 +27,6 @@
 (defmethod parse-adq :amount [m] (numeric-attr m))
 (defmethod parse-adq :cr [m] (numeric-attr m))
 (defmethod parse-adq :percentage [m] (numeric-attr m))
-;(defmethod parse-adq :modifier [m] (numeric-attr m))
 
 (defmethod parse-adq :name [m] (string-attr m))
 (defmethod parse-adq :reference [m] (string-attr m))
@@ -43,10 +34,31 @@
 (defmethod parse-adq :affects [m] (string-attr m))
 (defmethod parse-adq :damage [m] (string-attr m))
 (defmethod parse-adq :usage [m] (string-attr m))
+(defmethod parse-adq :reach [m] (string-attr m))
+(defmethod parse-adq :parry [m] (string-attr m))
+(defmethod parse-adq :block [m] (string-attr m))
 (defmethod parse-adq :attribute [m] (string-attr m))
+(defmethod parse-adq :college_name [m] (string-attr m))
+(defmethod parse-adq :accuracy [m] (string-attr m))
+(defmethod parse-adq :range [m] (string-attr m))
+(defmethod parse-adq :rate_of_fire [m] (string-attr m))
+(defmethod parse-adq :recoil [m] (string-attr m))
+(defmethod parse-adq :location [m] (string-attr m))
 
 (defmethod parse-adq :type [{:keys [content]}]
   {:type (set (map cs/trim (cs/split (first content) #",")))})
+
+(defmethod parse-adq :skill_prereq [{:keys [content]}]
+  {:skill_prereq (map parse-adq content)})
+
+(defmethod parse-adq :advantage_prereq [{:keys [content]}]
+  {:advantage_prereq (map parse-adq content)})
+
+(defmethod parse-adq :attribute_prereq [{:keys [content]}]
+  {:attribute_prereq (map parse-adq content)})
+
+(defmethod parse-adq :spell_bonus [{:keys [content]}]
+  {:spell_bonus (map parse-adq content)})
 
 (defmethod parse-adq :specialization [{:keys [tag content attrs]}]
   ;how do these attrs work?
@@ -64,6 +76,11 @@
 
 (defmethod parse-adq :modifier [{:keys [content attrs]}]
   {:modifier (apply merge (map parse-adq content))})
+
+(defmethod parse-adq :ranged_weapon[{:keys [content attrs]}]
+  {:ranged-weapon (map parse-adq content)
+   ;(apply merge (map parse-adq content))
+   })
 
 (defmethod parse-adq :dr_bonus [{:keys [content attrs]}]
   {:dr_bonus (map parse-adq content)})
@@ -88,4 +105,4 @@
     {:default (map parse-adq content)}
     (prn tag)))
 
-(parse-adq x)
+;(parse-adq x)
