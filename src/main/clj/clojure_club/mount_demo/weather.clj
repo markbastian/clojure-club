19143cfd239b69fa4616d561ce7703579a8d7afe
markbastian <markbastian@gmail.com>
2017-05-12 08:53:48 -0600

    Mount examples.

--- src/main/clj/clojure_club/mount_demo/weather.clj
+++ src/main/clj/clojure_club/mount_demo/weather.clj
@@ -2,11 +2,13 @@
   (:require [mount.core :refer [defstate] :as mount]
             [cheshire.core :as ch]))
 
+;This could be test vs. production data, simple vs. complex, whatever....
 (def wunderground-url "http://api.wunderground.com/api/46b315c0cf22f273/conditions/q/ID/Boise.json")
 (def openweathermap-url "http://api.openweathermap.org/data/2.5/weather?q=Boise,ID&appid=f5f2079dd7f4a6c3b6730d3441efb2c2")
 
 (defn get-weather [url] (ch/parse-string (slurp url) true))
 
+;Note that normally you'd want the configuration to return data of the same shape.
 (defstate weather-service-url :start wunderground-url)
 
 (defn weather [] (ch/parse-string (slurp weather-service-url) true))
